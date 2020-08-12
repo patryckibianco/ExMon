2985e140cc88386d8b1cf098a386acc46d91b8b3
Patrycki Bianco <patrycki.nune@grupoboticario.com.br>
2020-08-12 00:14:44 -0300

    Include more unit tests

--- README.md
+++ README.md
@@ -1,29 +1,21 @@
-# ###### ExMon ######
+## ExMon 
 
-This is a funny turn based game project built with Elixir
+###### This is a funny turn based game made with Elixir
 
-To create a player to the game you need to provide some initial informations
+
+**To create a player to the game you need to provide some initial informations**
 
 ExMon.create_player(MOVE_AVERAGE, MOVE_RANDOM, MOVE_HEAL, PLAYER_NAME)
 
-MOVE_AVERAGE: Damage  18..25
-MOVE_RANDOM:  Damage  10..35
-MOVE_HEAL:    Restore 18..35
+* MOVE_AVERAGE: Damage  18..25
+* MOVE_RANDOM:  Damage  10..35
+* MOVE_HEAL:    Restore 18..35
 
-To run/test the game execute the following commands:
+**To run/test the game execute the following commands:**
 
-1: iex -S mix
+1. iex -S mix
+2. player = ExMon.create_player(:punch, :kick, :heal, "Horla") 
+3. ExMon.start_game(player)
+4. ExMon.make_move(:punch) or ExMon.make_move(:kick) or ExMon.make_move(:heal)
 
-# For attacks and heal choose the names you want... Horla parameter is a name, you can choose whatever you want
-2: player = ExMon.create_player(:punch, :kick, :heal, "Horla") 
-3: ExMon.start_game(player)
-
-# Call make_move function providing the attack parameter with name has been choosed
-4: ExMon.make_move(:punch) or ExMon.make_move(:kick) or ExMon.make_move(:heal)
-
-# ENJOY IT! #
-
-
-
-
-
+**ENJOY IT!**
--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,7 +1,64 @@
 defmodule ExMonTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+  alias ExMon.Player
 
-  test "greets the world" do
-    assert true == true
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %Player{
+        life: 100,
+        moves: %{move_avg: :punch, move_heal: :heal, move_rnd: :kick},
+        name: "Horla"
+      }
+
+      assert expected_response == ExMon.create_player(:punch, :kick, :heal, "Horla")
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game starts return a message" do
+      player = Player.build(:punch, :kick, :heal, "Horla")
+
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
+
+      assert messages =~ "The battle has started"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build(:punch, :kick, :heal, "Horla")
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      :ok
+    end
+
+    test "when the move is valid do the move and computer makes a move" do
+      messages =
+        capture_io(fn ->
+          assert ExMon.make_move(:punch) == :ok
+        end)
+
+      assert messages =~ "Player attacks enemy"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "status: :continue"
+      assert messages =~ "turn: :player"
+    end
+
+    test "when the move is invalid returns a message" do
+      messages =
+        capture_io(fn ->
+          assert ExMon.make_move(:wrong) == :ok
+        end)
+
+      assert messages =~ "Oh oh! Looks that you're trying to do an inexistent move: wrong"
+    end
   end
 end
